ee385a9541e93a351e90c0fdf828649b4d846802
VG-1 <dd918w8jdu@163.com>
2024-06-28 16:35:26 +0800

    Add files via upload

---
+++ 造船寻宝.lua
@@ -0,0 +1,331 @@
+local IMAGE = "rbxassetid://18216647696"
+local Positions = UDim2.new(0.822025776, 0, 0.0401606411, 0)
+local Sizes = UDim2.new(0, 76, 0, 70)
+
+local KINGHUBMOBILE = Instance.new("ScreenGui")
+local _100x100 = Instance.new("Frame")
+local ImageButton = Instance.new("ImageButton")
+
+KINGHUBMOBILE.Name = "King Script"
+KINGHUBMOBILE.Parent = game:WaitForChild("CoreGui")
+KINGHUBMOBILE.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+_100x100.Name = "100x100"
+_100x100.Parent = KINGHUBMOBILE
+_100x100.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_100x100.Position = Positions
+_100x100.Size = UDim2.new(0, 76, 0, 70)
+
+ImageButton.Parent = _100x100
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.Size = Sizes
+ImageButton.Image = IMAGE
+ImageButton.MouseButton1Down:connect(function()
+        local vim = game:service("VirtualInputManager")
+        vim:SendKeyEvent(true, "RightControl", false, game)
+
+        local vim = game:service("VirtualInputManager")
+        vim:SendKeyEvent(false, "RightControl", false, game)
+end)
+
+local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hunryn/VB/main/libKINGuI.lUa"))()
+local win = UILib:Window("King Script丨造船寻宝",Color3.fromRGB(102, 255, 153), Enum.KeyCode.RightControl)
+
+local setup = win:Tab("介绍")
+    setup:Label("King客户名:"..game.Players.LocalPlayer.Character.Name)
+    setup:Label("King授权的注入器:"..identifyexecutor())
+    setup:Label("King谢谢支持")
+    setup:Label("副作者:龙叔【别被冒充的龙叔的圈了】")
+    setup:Label("帮助者:白貓，钢筋，无xia，无极，清岩")
+    setup:Label("本次开启为造船寻宝脚本")
+
+    local Tab = win:Tab("主要的功能")
+    local Tab2 = win:Tab("娱乐功能")
+    local Tab3 = win:Tab("角色功能")
+    local King = win:Tab("作者信息")
+
+Tab:Toggle("自动刷金币", "text", false, function(FARM)
+        _G.FARMs = FARM
+        while _G.FARMs do
+            wait()
+            pcall(
+                function()
+                    game.Workspace.Gravity = 0
+                    wait(0.5)
+                    local TweenService = game:GetService("TweenService")
+                    local Tw =
+                        TweenService:Create(
+                        game.Players.LocalPlayer.Character.HumanoidRootPart,
+                        TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0),
+                        {
+                            CFrame = CFrame.new(
+                                -51.05019,
+                                43.5682182,
+                                702.193481,
+                                -0.999886811,
+                                -0.000135422233,
+                                0.0150433034,
+                                8.65181704e-09,
+                                0.999959469,
+                                0.00900237076,
+                                -0.0150439134,
+                                0.00900135189,
+                                -0.999846339
+                            )
+                        }
+                    ):Play()
+                    wait(1)
+                    local TweenService = game:GetService("TweenService")
+                    local Tw =
+                        TweenService:Create(
+                        game.Players.LocalPlayer.Character.HumanoidRootPart,
+                        TweenInfo.new(30, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0),
+                        {
+                            CFrame = CFrame.new(
+                                -50.4467354,
+                                34.108551,
+                                8676.95117,
+                                -0.999937415,
+                                -0.000405743311,
+                                0.0111814411,
+                                -6.1212857e-09,
+                                0.999342263,
+                                0.0362627953,
+                                -0.0111888004,
+                                0.0362605266,
+                                -0.999279737
+                            )
+                        }
+                    ):Play()
+                    wait(30)
+                    local TweenService = game:GetService("TweenService")
+                    local Tw =
+                        TweenService:Create(
+                        game.Players.LocalPlayer.Character.HumanoidRootPart,
+                        TweenInfo.new(5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0),
+                        {
+                            CFrame = CFrame.new(
+                                -54.1517258,
+                                -359.077667,
+                                9497.03418,
+                                -0.99982208,
+                                0.00359633542,
+                                -0.0185163822,
+                                -7.16419102e-09,
+                                0.981655717,
+                                0.190661997,
+                                0.0188623965,
+                                0.190628082,
+                                -0.981481075
+                            )
+                        }
+                    ):Play()
+                    wait(5)
+                    game.Workspace.Gravity = 200
+                    wait(20)
+                end
+            )
+        end
+    end)
+Tab:Toggle("自动刷金币快速版", "text", false, function(FARM2)
+        _G.FARM2s = FARM2
+        while _G.FARM2s do
+            wait()
+            pcall(
+                function()
+                    game.Workspace.Gravity = 0
+                    wait(0.5)
+                    local TweenService = game:GetService("TweenService")
+                    local Tw =
+                        TweenService:Create(
+                        game.Players.LocalPlayer.Character.HumanoidRootPart,
+                        TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0),
+                        {
+                            CFrame = CFrame.new(
+                                -51.05019,
+                                43.5682182,
+                                702.193481,
+                                -0.999886811,
+                                -0.000135422233,
+                                0.0150433034,
+                                8.65181704e-09,
+                                0.999959469,
+                                0.00900237076,
+                                -0.0150439134,
+                                0.00900135189,
+                                -0.999846339
+                            )
+                        }
+                    ):Play()
+                    wait(1)
+                    local TweenService = game:GetService("TweenService")
+                    local Tw =
+                        TweenService:Create(
+                        game.Players.LocalPlayer.Character.HumanoidRootPart,
+                        TweenInfo.new(5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0),
+                        {
+                            CFrame = CFrame.new(
+                                -50.4467354,
+                                34.108551,
+                                8676.95117,
+                                -0.999937415,
+                                -0.000405743311,
+                                0.0111814411,
+                                -6.1212857e-09,
+                                0.999342263,
+                                0.0362627953,
+                                -0.0111888004,
+                                0.0362605266,
+                                -0.999279737
+                            )
+                        }
+                    ):Play()
+                    wait(5)
+                    local TweenService = game:GetService("TweenService")
+                    local Tw =
+                        TweenService:Create(
+                        game.Players.LocalPlayer.Character.HumanoidRootPart,
+                        TweenInfo.new(5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0),
+                        {
+                            CFrame = CFrame.new(
+                                -51.05019,
+                                43.5682182,
+                                702.193481,
+                                -0.999886811,
+                                -0.000135422233,
+                                0.0150433034,
+                                8.65181704e-09,
+                                0.999959469,
+                                0.00900237076,
+                                -0.0150439134,
+                                0.00900135189,
+                                -0.999846339
+                            )
+                        }
+                    ):Play()
+                    wait(5)
+                    local Event = game:GetService("Workspace").ClaimRiverResultsGold
+                    Event:FireServer()
+                    wait(0.1)
+                    game.Players.LocalPlayer.Character.Humanoid.Health = -1000
+                    wait(0.1)
+                    game.Workspace.Gravity = 200
+                    wait(7)
+                end
+            )
+        end
+    end)
+Tab:Toggle(
+    "自动索赔金币", "text", false, function(GlaimGold)
+        _G.ClaimGolds = ClaimGold
+        while _G.ClaimGolds do
+            wait()
+            pcall(
+                function()
+                    local Event = game:GetService("Workspace").ClaimRiverResultsGold
+                    Event:FireServer()
+                end
+            )
+        end
+
+        MainSection:Button(
+            "ClearAllBoatParts",
+            function()
+                local Event = game:GetService("Workspace").ClearAllPlayersBoatParts
+                Event:FireServer()
+            end
+        )
+
+        wait(0.5)
+        local ba = Instance.new("ScreenGui")
+        local ca = Instance.new("TextLabel")
+        local da = Instance.new("Frame")
+        local _b = Instance.new("TextLabel")
+        local ab = Instance.new("TextLabel")
+        ba.Parent = game.CoreGui
+        ba.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+        ca.Parent = ba
+        ca.Active = true
+        ca.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+        ca.Draggable = true
+        ca.Position = UDim2.new(0.698610067, 0, 0.098096624, 0)
+        ca.Size = UDim2.new(0, 370, 0, 52)
+        ca.Font = Enum.Font.SourceSansSemibold
+        ca.Text = "Anti AFK Script"
+        ca.TextColor3 = Color3.new(0, 1, 1)
+        ca.TextSize = 22
+        da.Parent = ca
+        da.BackgroundColor3 = Color3.new(0.196078, 0.196078, 0.196078)
+        da.Position = UDim2.new(0, 0, 1.0192306, 0)
+        da.Size = UDim2.new(0, 370, 0, 107)
+        _b.Parent = da
+        _b.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+        _b.Position = UDim2.new(0, 0, 0.800455689, 0)
+        _b.Size = UDim2.new(0, 370, 0, 21)
+        _b.Font = Enum.Font.Arial
+        _b.Text = "made by simon"
+        _b.TextColor3 = Color3.new(0, 1, 1)
+        _b.TextSize = 20
+        ab.Parent = da
+        ab.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+        ab.Position = UDim2.new(0, 0, 0.158377, 0)
+        ab.Size = UDim2.new(0, 370, 0, 44)
+        ab.Font = Enum.Font.ArialBold
+        ab.Text = "Status: Active"
+        ab.TextColor3 = Color3.new(0, 1, 1)
+        ab.TextSize = 20
+
+        local bb = game:service "VirtualUser"
+        game:service "Players".LocalPlayer.Idled:connect(
+            function()
+                bb:CaptureController()
+                bb:ClickButton2(Vector2.new())
+                ab.Text = "机器人想踢你，但我踢了他"
+                wait(2)
+                ab.Text = "状态：Active"
+            end
+        )
+    end)
+Tab2:Button("无限方块（假）", function()
+        spawn(
+            function()
+                local guiinf =
+                    game:GetService("Players").LocalPlayer.PlayerGui.BuildGui.InventoryFrame.ScrollingFrame.BlocksFrame
+                _G.Blocks = false
+                while game:GetService("RunService").RenderStepped:wait() do
+                    if _G.Blocks then
+                        return
+                    end
+                    for i, v in pairs(guiinf:GetDescendants()) do
+                        if v.Name == "AmountText" then
+                            v.Text = math.huge
+                        end
+                    end
+                end
+            end
+        )
+    end)
+    
+Tab3:Button("变🐔鸡", function()
+        local players = game:GetService("Players")
+        local client = players.LocalPlayer
+        local Chicken = workspace:WaitForChild("ChangeCharacter")
+        Chicken:FireServer("ChickenCharacter")
+    end)
+Tab3:Button("变🐧企鹅", function()
+        local players = game:GetService("Players")
+        local client = players.LocalPlayer
+        local penguin = workspace:WaitForChild("ChangeCharacter")
+        penguin:FireServer("PenguinCharacter")
+    end)
+Tab3:Button("变🦊狐狸", function()
+        local players = game:GetService("Players")
+        local client = players.LocalPlayer
+        local fox = workspace:WaitForChild("ChangeCharacter")
+        fox:FireServer("FoxCharacter")
+    end)
+    King:Label("King就是霖溺")
+
+King:Button("作者QQ群，点击我复制", function()
+    setclipboard("霖溺QQ新主群https://qm.qq.com/q/SU0hmhIvwk")
+end)
