90f4626187d866079e694c1ad15a0824c5e5db5c
VG-1 <dd918w8jdu@163.com>
2024-06-28 15:51:23 +0800

    Add files via upload

---
+++ 举重模拟器.lua
@@ -0,0 +1,67 @@
+local IMAGE = "rbxassetid://18216647696"
+local Positions = UDim2.new(0.822025776, 0, 0.0401606411, 0)
+local Sizes = UDim2.new(0, 76, 0, 70)
+
+local KINGHUBMOBILE = Instance.new("ScreenGui")
+local _100x100 = Instance.new("Frame")
+local ImageButton = Instance.new("ImageButton")
+
+KINGHUBMOBILE.Name = "King Script"
+KINGHUBMOBILE.Parent = game:WaitForChild("CoreGui")
+KINGHUBMOBILE.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+_100x100.Name = "100x100"
+_100x100.Parent = KINGHUBMOBILE
+_100x100.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_100x100.Position = Positions
+_100x100.Size = UDim2.new(0, 76, 0, 70)
+
+ImageButton.Parent = _100x100
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.Size = Sizes
+ImageButton.Image = IMAGE
+ImageButton.MouseButton1Down:connect(function()
+        local vim = game:service("VirtualInputManager")
+        vim:SendKeyEvent(true, "RightControl", false, game)
+
+        local vim = game:service("VirtualInputManager")
+        vim:SendKeyEvent(false, "RightControl", false, game)
+end)
+
+local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hunryn/VB/main/libKINGuI.lUa"))()
+local win = UILib:Window("King Script丨举重模拟器",Color3.fromRGB(102, 255, 153), Enum.KeyCode.RightControl)
+
+local setup = win:Tab("介绍")
+    setup:Label("King客户名:"..game.Players.LocalPlayer.Character.Name)
+    setup:Label("King授权的注入器:"..identifyexecutor())
+    setup:Label("King谢谢支持")
+    setup:Label("副作者:龙叔【别被冒充的龙叔的圈了】")
+    setup:Label("帮助者:白貓，钢筋，无xia，无极，清岩")
+    setup:Label("本次开启为举重模拟器脚本")
+
+local tab = win:Tab("主要功能")
+
+tab:Toggle("自动举重", false, function(Value)
+toggle = Value
+while toggle do wait()
+local ohTable1 = {
+	[1] = "GainMuscle"
+}
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(ohTable1)
+    end
+end)
+
+tab:Toggle("自动售卖", false, function(Value)
+    tog = Value
+   while tog do wait()
+local ohTable1 = {
+	[1] = "SellMuscle"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer(ohTable1)
+    end
+end)
+
+tab:Button("远程打开商店", function()
+game:GetService("Players").LocalPlayer.PlayerGui["Main_Gui"]["UpgradeMenu_Frame"].Visible = true
+end)
