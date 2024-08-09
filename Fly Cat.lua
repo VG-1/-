7896a18d6b495e73dbf768fa3938960773f3b89c
VG-1 <dd918w8jdu@163.com>
2024-08-09 19:13:31 +0800

    Update Fly Cat.lua
    ˚*̥(∗*⁰͈꒨⁰͈)*̥哇哦~，飞车

--- Fly Cat.lua
+++ Fly Cat.lua
@@ -1,7 +1,10 @@
- --[[究极改装]]
-local KingScriptFly = Instance.new("ScreenGui")
+--MADE BY WARRIOR ROBERR
+-- Version: 3.2
+
+-- Instances:
+local Flymguiv2 = Instance.new("ScreenGui")
 local Drag = Instance.new("Frame")
-local KingFlyCatname = Instance.new("Frame")
+local FlyFrame = Instance.new("Frame")
 local ddnsfbfwewefe = Instance.new("TextButton")
 local Speed = Instance.new("TextBox")
 local Fly = Instance.new("TextButton")
@@ -16,43 +19,44 @@ local Flyon = Instance.new("Frame")
 local W = Instance.new("TextButton")
 local S = Instance.new("TextButton")
 
+--Properties:
 
-KingScriptFly.Name = "King Script Fly"
-KingScriptFly.Parent = game.CoreGui
-KingScriptFly.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Flymguiv2.Name = "Flym gui v2"
+Flymguiv2.Parent = game.CoreGui
+Flymguiv2.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Drag.Name = "Drag"
-Drag.Parent = KingScriptFly
+Drag.Parent = Flymguiv2
 Drag.Active = true
-Drag.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Drag.BackgroundColor3 = Color3.fromRGB(0, 150, 191)
 Drag.BorderSizePixel = 0
 Drag.Draggable = true
 Drag.Position = UDim2.new(0.482438415, 0, 0.454874992, 0)
 Drag.Size = UDim2.new(0, 237, 0, 27)
 
-KingFlyCatname.Name = "KingFlyCatname"
-KingFlyCatname.Parent = Drag
-KingFlyCatname.BackgroundColor3 = Color3.fromRGB(102, 255, 153)
-KingFlyCatname.BorderSizePixel = 0
-KingFlyCatname.Draggable = true
-KingFlyCatname.Position = UDim2.new(-0.00200000009, 0, 0.989000022, 0)
-KingFlyCatname.Size = UDim2.new(0, 237, 0, 139)
+FlyFrame.Name = "FlyFrame"
+FlyFrame.Parent = Drag
+FlyFrame.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
+FlyFrame.BorderSizePixel = 0
+FlyFrame.Draggable = true
+FlyFrame.Position = UDim2.new(-0.00200000009, 0, 0.989000022, 0)
+FlyFrame.Size = UDim2.new(0, 237, 0, 139)
 
 ddnsfbfwewefe.Name = "ddnsfbfwewefe"
-ddnsfbfwewefe.Parent = KingFlyCatname
+ddnsfbfwewefe.Parent = FlyFrame
 ddnsfbfwewefe.BackgroundColor3 = Color3.fromRGB(0, 150, 191)
 ddnsfbfwewefe.BorderSizePixel = 0
 ddnsfbfwewefe.Position = UDim2.new(-0.000210968778, 0, -0.00395679474, 0)
 ddnsfbfwewefe.Size = UDim2.new(0, 237, 0, 27)
 ddnsfbfwewefe.Font = Enum.Font.SourceSans
-ddnsfbfwewefe.Text = "King Fly Cat"
+ddnsfbfwewefe.Text = "King飞车"
 ddnsfbfwewefe.TextColor3 = Color3.fromRGB(255, 255, 255)
 ddnsfbfwewefe.TextScaled = true
 ddnsfbfwewefe.TextSize = 14.000
 ddnsfbfwewefe.TextWrapped = true
 
-Speed.Name = "输入数值"
-Speed.Parent = KingFlyCatname
+Speed.Name = "数值"
+Speed.Parent = FlyFrame
 Speed.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
 Speed.BorderColor3 = Color3.fromRGB(0, 0, 191)
 Speed.BorderSizePixel = 0
@@ -67,21 +71,21 @@ Speed.TextSize = 14.000
 Speed.TextWrapped = true
 
 Fly.Name = "Fly"
-Fly.Parent = KingFlyCatname
+Fly.Parent = FlyFrame
 Fly.BackgroundColor3 = Color3.fromRGB(0, 150, 191)
 Fly.BorderSizePixel = 0
 Fly.Position = UDim2.new(0.0759493634, 0, 0.705797076, 0)
 Fly.Size = UDim2.new(0, 199, 0, 32)
 Fly.Font = Enum.Font.SourceSans
-Fly.Text = "开启"
-Fly.TextColor3 = Color3.fromRGB(102, 255, 153)
+Fly.Text = "开启飞车"
+Fly.TextColor3 = Color3.fromRGB(255, 255, 255)
 Fly.TextScaled = true
 Fly.TextSize = 14.000
 Fly.TextWrapped = true
 Fly.MouseButton1Click:Connect(function()
 	local HumanoidRP = game.Players.LocalPlayer.Character.HumanoidRootPart
 	Fly.Visible = false
-	Stat2.Text = "开启"
+	Stat2.Text = "On"
 	Stat2.TextColor3 = Color3.fromRGB(0, 255, 0)
 	Unfly.Visible = true
 	Flyon.Visible = true
@@ -97,55 +101,55 @@ Fly.MouseButton1Click:Connect(function()
 end)
 
 Speeed.Name = "Speeed"
-Speeed.Parent = KingFlyCatname
+Speeed.Parent = FlyFrame
 Speeed.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
 Speeed.BorderSizePixel = 0
 Speeed.Position = UDim2.new(0.0759493634, 0, 0.402877688, 0)
 Speeed.Size = UDim2.new(0, 87, 0, 32)
 Speeed.ZIndex = 0
 Speeed.Font = Enum.Font.SourceSans
-Speeed.Text = "速度:"
+Speeed.Text = "Speed:"
 Speeed.TextColor3 = Color3.fromRGB(255, 255, 255)
 Speeed.TextScaled = true
 Speeed.TextSize = 14.000
 Speeed.TextWrapped = true
 
-Stat.Name = "状态"
-Stat.Parent = KingFlyCatname
+Stat.Name = "Stat"
+Stat.Parent = FlyFrame
 Stat.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
 Stat.BorderSizePixel = 0
 Stat.Position = UDim2.new(0.299983799, 0, 0.239817441, 0)
 Stat.Size = UDim2.new(0, 85, 0, 15)
 Stat.Font = Enum.Font.SourceSans
-Stat.Text = "状态:"
+Stat.Text = "Status:"
 Stat.TextColor3 = Color3.fromRGB(255, 255, 255)
 Stat.TextScaled = true
 Stat.TextSize = 14.000
 Stat.TextWrapped = true
 
 Stat2.Name = "Stat2"
-Stat2.Parent = KingFlyCatname
+Stat2.Parent = FlyFrame
 Stat2.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
 Stat2.BorderSizePixel = 0
 Stat2.Position = UDim2.new(0.546535194, 0, 0.239817441, 0)
 Stat2.Size = UDim2.new(0, 27, 0, 15)
 Stat2.Font = Enum.Font.SourceSans
-Stat2.Text = "关闭"
+Stat2.Text = "Off"
 Stat2.TextColor3 = Color3.fromRGB(255, 0, 0)
 Stat2.TextScaled = true
 Stat2.TextSize = 14.000
 Stat2.TextWrapped = true
 
 Unfly.Name = "Unfly"
-Unfly.Parent = KingFlyCatname
+Unfly.Parent = FlyFrame
 Unfly.BackgroundColor3 = Color3.fromRGB(0, 150, 191)
 Unfly.BorderSizePixel = 0
 Unfly.Position = UDim2.new(0.0759493634, 0, 0.705797076, 0)
 Unfly.Size = UDim2.new(0, 199, 0, 32)
 Unfly.Visible = false
 Unfly.Font = Enum.Font.SourceSans
-Unfly.Text = "正在飞行"
-Unfly.TextColor3 = Color3.fromRGB(255, 0, 0)
+Unfly.Text = "Disable"
+Unfly.TextColor3 = Color3.fromRGB(255, 255, 255)
 Unfly.TextScaled = true
 Unfly.TextSize = 14.000
 Unfly.TextWrapped = true
@@ -167,8 +171,8 @@ Vfly.BackgroundColor3 = Color3.fromRGB(0, 150, 191)
 Vfly.BorderSizePixel = 0
 Vfly.Size = UDim2.new(0, 57, 0, 27)
 Vfly.Font = Enum.Font.SourceSans
-Vfly.Text = "Fly Cat"
-Vfly.TextColor3 = Color3.fromRGB(255, 0, 0)
+Vfly.Text = "霖溺"
+Vfly.TextColor3 = Color3.fromRGB(255, 255, 255)
 Vfly.TextScaled = true
 Vfly.TextSize = 14.000
 Vfly.TextWrapped = true
@@ -180,40 +184,40 @@ Close.BorderSizePixel = 0
 Close.Position = UDim2.new(0.875, 0, 0, 0)
 Close.Size = UDim2.new(0, 27, 0, 27)
 Close.Font = Enum.Font.SourceSans
-Close.Text = "关"
-Close.TextColor3 = Color3.fromRGB(255, 0, 0)
+Close.Text = "X"
+Close.TextColor3 = Color3.fromRGB(255, 255, 255)
 Close.TextScaled = true
 Close.TextSize = 14.000
 Close.TextWrapped = true
 Close.MouseButton1Click:Connect(function()
-	KingScriptFly:Destroy()
+	Flymguiv2:Destroy()
 end)
 
 Minimize.Name = "Minimize"
 Minimize.Parent = Drag
-Minimize.BackgroundColor3 = Color3.fromRGB(102, 255, 153)
+Minimize.BackgroundColor3 = Color3.fromRGB(0, 150, 191)
 Minimize.BorderSizePixel = 0
 Minimize.Position = UDim2.new(0.75, 0, 0, 0)
 Minimize.Size = UDim2.new(0, 27, 0, 27)
 Minimize.Font = Enum.Font.SourceSans
 Minimize.Text = "-"
-Minimize.TextColor3 = Color3.fromRGB(255, 0, 0)
+Minimize.TextColor3 = Color3.fromRGB(255, 255, 255)
 Minimize.TextScaled = true
 Minimize.TextSize = 14.000
 Minimize.TextWrapped = true
 function Mini()
-	if Minimize.Text == "缩" then
-		Minimize.Text = "放"
-		KingFlyCatname.Visible = false
-	elseif Minimize.Text == "放" then
-		Minimize.Text = "缩"
-		KingFlyCatname.Visible = true
+	if Minimize.Text == "-" then
+		Minimize.Text = "+"
+		FlyFrame.Visible = false
+	elseif Minimize.Text == "+" then
+		Minimize.Text = "-"
+		FlyFrame.Visible = true
 	end
 end
 Minimize.MouseButton1Click:Connect(Mini)
 
 Flyon.Name = "Fly on"
-Flyon.Parent = KingScriptFly
+Flyon.Parent = Flymguiv2
 Flyon.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Flyon.BorderSizePixel = 0
 Flyon.Position = UDim2.new(0.117647067, 0, 0.550284624, 0)
@@ -229,8 +233,8 @@ W.BorderSizePixel = 0
 W.Position = UDim2.new(0.134719521, 0, 0.0152013302, 0)
 W.Size = UDim2.new(0.708999991, 0, 0.499000013, 0)
 W.Font = Enum.Font.SourceSans
-W.Text = "前"
-W.TextColor3 = Color3.fromRGB(255, 0, 0)
+W.Text = "^"
+W.TextColor3 = Color3.fromRGB(255, 255, 255)
 W.TextScaled = true
 W.TextSize = 14.000
 W.TextWrapped = true
@@ -292,8 +296,8 @@ S.Position = UDim2.new(0.134000003, 0, 0.479999989, 0)
 S.Rotation = 180.000
 S.Size = UDim2.new(0.708999991, 0, 0.499000013, 0)
 S.Font = Enum.Font.SourceSans
-S.Text = "后"
-S.TextColor3 = Color3.fromRGB(255, 0, 0)
+S.Text = "^"
+S.TextColor3 = Color3.fromRGB(255, 255, 255)
 S.TextScaled = true
 S.TextSize = 14.000
 S.TextWrapped = true
